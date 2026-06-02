82bb64c2831e135be0af00e68c1f3f0d636e396a
Jemery <jere.mendez@duocuc.cl>
2026-06-02 19:55:57 -0400

    Add files via upload

---
+++ contactos/src/main/java/com/multigacha/contactos/ContactosApplication.java
@@ -0,0 +1,14 @@
+package com.multigacha.contactos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+@EnableFeignClients
+@SpringBootApplication
+public class ContactosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactosApplication.class, args);
+	}
+
+}
