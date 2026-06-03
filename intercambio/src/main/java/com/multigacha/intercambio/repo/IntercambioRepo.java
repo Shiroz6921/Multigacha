cace0e539f83a11c64e22cdacea123efaf7d9048
Shiroz6921 <lui.urrea@duocuc.cl>
2026-06-02 21:24:59 -0400

    ,

--- intercambio/src/main/java/com/multigacha/intercambio/repo/IntercambioRepo.java
+++ intercambio/src/main/java/com/multigacha/intercambio/repo/IntercambioRepo.java
@@ -3,15 +3,8 @@ package com.multigacha.intercambio.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.multigacha.intercambio.model.Intercambio;
-import com.multigacha.intercambio.model.ProductoCliente;
-
-
 
 @Repository
 public interface IntercambioRepo extends JpaRepository<Intercambio, Integer> {
-
-    ProductoCliente findByIdProducto(Integer idProducto); 
-
-    Integer findIdProductoById(Integer id);
 
 }
--- intercambio/src/main/java/com/multigacha/intercambio/service/IntercambioService.java
+++ intercambio/src/main/java/com/multigacha/intercambio/service/IntercambioService.java
@@ -1,32 +1,32 @@
 package com.multigacha.intercambio.service;
 
 import java.util.Date;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.multigacha.intercambio.dto.IntercambioRequestDTO;
 import com.multigacha.intercambio.model.Intercambio;
 import com.multigacha.intercambio.model.ProductoCliente;
 import com.multigacha.intercambio.repo.IntercambioRepo;
+import com.multigacha.intercambio.repo.ProductoClienteRepo;
+
 
 @Service
 public class IntercambioService {
     
     @Autowired
     private IntercambioRepo repo1;
-    
+
     @Autowired
-    private ProductoCliente repo2;
+    private ProductoClienteRepo repo2;
 
 
 
 
-    public Intercambio crearIntercambio (IntercambioRequestDTO dto) {
+    public Intercambio crearIntercambio(IntercambioRequestDTO dto) {
         ProductoCliente inventarioA = repo2.findByIdProducto(dto.getIdProductoA());
         ProductoCliente inventarioB = repo2.findByIdProducto(dto.getIdProductoB());
 
-        if (inventarioA == null || inventarioB == null){
+        if (inventarioA == null || inventarioB == null) {
             throw new RuntimeException("Uno o ambos productos no se encuentran");
         }
 
@@ -34,16 +34,15 @@ public class IntercambioService {
         inventarioA.setIdCliente(inventarioB.getIdCliente());
         inventarioB.setIdCliente(clienteTemp);
 
-        repo2.save(inventarioB);  
-        repo2.save(inventarioA);  
+        repo2.save(inventarioB);
+        repo2.save(inventarioA);
 
         Intercambio intercambio = new Intercambio();
         intercambio.setFecha(new Date());
         intercambio.setTraspaso(inventarioA);
-        repo1.save(intercambio); 
+        repo1.save(intercambio);
 
         return intercambio;
-
     }
 
 
